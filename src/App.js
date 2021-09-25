81150007825a1ebf8989d44db4096762733741f3
Carlos Henrique <rapaiva21@gmail.com>
2021-09-25 18:25:35 -0300

    Programa de Gerenciamento de Notas com alguns elementos HTML em App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
 function App() {
   return (
-   <h1> Meu app React</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota..."/>
+        <button>Criar Nota</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </section>
   );
 }
 
