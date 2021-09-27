8c287a14f01d92eb13e0868754042ec3ec82122d
Carlos Henrique <rapaiva21@gmail.com>
2021-09-27 19:14:06 -0300

    Adicionados as classes para serem chamados o arquivo css.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-
-
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
   render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
     );
   }
 }
-
 
 export default App;
